f595d849c8e2c78994684b0c42c848b38ce62e6f
Samuel Kozuch <samuel.kozuch@keboola.com>
2019-07-24 21:54:18 +0200

    Version check

--- src/main.py
+++ src/main.py
@@ -36,7 +36,7 @@ GD_CUSTOM_DOMAIN = 'domain_custom'
 GD_URL = 'gd_url'
 KBC_URL = 'provisioning_url'
 
-APP_VERSION = '0.1.6'
+APP_VERSION = '0.1.7'
 
 MANDATORY_PARS = [GD_USERNAME, GD_PASSWORD, GD_PID, GD_CUSTOM_DOMAIN]
 
